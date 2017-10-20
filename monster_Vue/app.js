cd86d2ddf41acb06fc4397aa2e7c5860fcc7e114
Rosa Dolson <rosadolson@Rosas-MacBook-Pro.local>
2017-10-19 20:01:54 -0600

    Monster Vue

---
+++ monster_Vue/app.js
@@ -0,0 +1,14 @@
+function Monster(name, diet, img){
+    this.name = name
+    this.diet = diet
+    this.img = img
+}
+
+const monsters = []
+
+const jason = new Monster('Jason Voorhees', ['Monster Energy'], 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5Jo_OgF4rJaizQPPbyIqUvFJQvlKgx2E3coPBoZuS80sKTWyl-g')
+const frankenstein = new Monster('Frankenstein',['small children', 'annoying villagers'], 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy82J3vzRVULkATH_UBy9M7a49ix3OVLrCSyawyJLqSNH55kBh')
+const yeti = new Monster('Abominable Snowman',['snow cones, stray skiers'], 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScRq5qEVJxCAdX2H2ALdL1ErMTZI-MX5ELqArSYuGpoWd52HlbaA')
+const dracula = new Monster('Dracula',['blood', 'expensive wine'], 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_Imy2Lvr2WLHs9Rj7CM4ISLdgN_6LDe_IQe0WTSzgUkEVwJpH')
+const mikeWazowski = new Monster('Mike Wazowski',['not snow cones', 'giggles'], 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEbu1MsstJRnCdRRXTvJFhlvTtBxxuh0fh16EjOU8EqH3SDc_y')
+
